5ebe9093898957de32f4c77a78b4273c4be118de
root <root@clubpenguingenerations.me>
2017-07-27 15:23:42 +0200

    removed folder

--- Register/includes/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$siteName = 'CPPS Name';
-
-$database = new PDO('mysql:host=localhost;dbname=sweater', 'root', "");
-
- ?>
--- Register/includes/footer.php
+++
@@ -1,5 +0,0 @@
-    </div>
-    <br /><br /><br /><br />
-    <center><small>Your footer goes here.</small></center>
-  </body>
-</html>
